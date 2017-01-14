0464eb0d5c1f90db5e31d60d0613526cc1e58d7c
lakhera2017 <laprashant@gmail.com>
2017-01-14 13:29:46 -0500

    finding smallest of three number

---
+++ smallestofthreenum.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3.6
+a = 10
+b = 20
+c = 30
+
+if a < b and a < c:
+    print ("a is smallest")
+elif b < c:
+    print ("b is smallest")
+else:
+    print ("c is smallest")
---
+++ testing.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3.6
+
+a = 10
+
+if a == 11:
+    print("Value of a: " + a)
+print("Goodbye")
